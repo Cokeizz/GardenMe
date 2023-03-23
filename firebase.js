d7448adfabdad1b95830ede4fa30bc71a73f99e4
Cokeizz <akrd.akrd@kkumail.com>
2023-03-24 05:13:23 +0700

    Login With Email Completed

---
+++ firebase.js
@@ -0,0 +1,31 @@
+// Import the functions you need from the SDKs you need
+// v9 compat packages are API compatible with v8 code
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA6xzr6WKrZUDHyQYaLqiZ-0fL0znxs5hI",
+  authDomain: "gardenme-963b3.firebaseapp.com",
+  projectId: "gardenme-963b3",
+  storageBucket: "gardenme-963b3.appspot.com",
+  messagingSenderId: "691793034488",
+  appId: "1:691793034488:web:814048f1027b8122e4cf0d",
+  measurementId: "G-3DYQMKNVKG"
+};
+
+// Initialize Firebase
+let app;
+if (firebase.apps.length === 0) {
+  app = firebase.initializeApp(firebaseConfig);
+} else {
+  app = firebase.app()
+}
+
+const auth = firebase.auth()
+
+export { auth };
